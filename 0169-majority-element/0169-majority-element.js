9ad83480adab3b109ad884792406d4a18d1d35ab
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-10 16:33:19 +0530

    Time: 0 ms (100%), Space: 56.8 MB (63.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    var candidate=null;
+    var candidate=0;
     var count=0;
 
     for(var i=0;i<nums.length;i++){
-        if(count === 0) {candidate=nums[i];
+        if(count === 0) {
+            candidate=nums[i];
         }
-        if(nums[i]=== candidate) {
-            count++;
-        }
-        else{
-            count--;
-        }
+        if(nums[i]=== candidate) count++;
+
+        else count--
     }
-    return candidate
+
+    return candidate;
 
 };
