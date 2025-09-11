ff474b6b4070358e67311211db367876b86a987d
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-11 12:53:27 +0530

    Time: 12 ms (92.77%), Space: 74.3 MB (39.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,18 +4,27 @@
  */
 var containsDuplicate = function(nums) {
 
-      var arr = nums.sort((a, b) => a - b); 
+    // var arr = nums.sort((a, b) => a - b); 
 
-    
-    for(var i=0;i<=nums.length-1;i++)
-    {
-            if(nums[i] === nums[i+1])
-            {
-                return true
-            }
+    // for(var i=0;i<=nums.length-1;i++)
+    // {
+    //         if(nums[i] === nums[i+1])
+    //         {
+    //             return true
+    //         }
 
         
+    // }
+    // return false
+
+    var seen = new Set();
+
+    for(var x of nums){
+        if(seen.has(x)){
+            return true
+        }
+        seen.add(x)
     }
+
     return false
-    
 };
