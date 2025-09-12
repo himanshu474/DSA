7e2677da50aa46f6b77bca6e0130b06ff9aa486e
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-12 12:20:25 +0530

    Time: 32 ms (47.35%), Space: 81.3 MB (47.72%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,22 +4,38 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-   const set = new Set();
+//    const set = new Set();
     
-    for (let i = 0; i < nums.length; i++) {
-        // Remove the element that is now outside the window
-        if (i > k) {
-            set.delete(nums[i - k - 1]);
-        }
+//     for (let i = 0; i < nums.length; i++) {
+//         // Remove the element that is now outside the window
+//         if (i > k) {
+//             set.delete(nums[i - k - 1]);
+//         }
         
-        // Check for duplicate in the current window
-        if (set.has(nums[i])) {
-            return true;
-        }
+//         // Check for duplicate in the current window
+//         if (set.has(nums[i])) {
+//             return true;
+//         }
         
-        // Add the current element to the window
-        set.add(nums[i]);
+//         // Add the current element to the window
+//         set.add(nums[i]);
+//     }
+    
+//     return false;
+
+const map= new Map();
+
+for(var i=0;i<nums.length;i++){
+    if(map.has(nums[i])){
+        const j= map.get(nums[i])
+    
+    if(Math.abs(j-i)<=k)
+    {
+        return true 
     }
-    
-    return false;
+    }
+    map.set(nums[i],i)
+
+}
+return false;
 };
