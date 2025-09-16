a085bd5e214ac2d0e3b6a7a046c18bff8cfc43c1
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-16 15:55:29 +0530

    Time: 8 ms (47.68%), Space: 61.5 MB (93.18%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -1,8 +1,16 @@
-/**
- * @param {number[]} nums
- */
 var NumArray = function(nums) {
-    this.nums=nums
+    
+    this.size = nums.length; //stores length of array
+    this.prefixSum = Array( nums.length ).fill( 0 );//empty array
+    this.prefixSum[0] = nums[0];//first value of prefix sum is the same as that of the first element of nums
+    
+    for( let i = 1; i < this.size ; i++ ){//traverse from left to right of array nums
+        this.prefixSum[i] = this.prefixSum[i-1] + nums[i];
+        /*stores sum of current value(nums) + previous sum value 
+        at the index before it(prefixsum)*/
+    }
+    
+    
 };
 
 /** 
@@ -11,17 +19,18 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(left, right) {
-    let sum=0;
-    for(var i=left;i<=right;i++){
-        sum+=this.nums[i]
+    
+    if( left == 0 ){
+        /* if left index isnt mentioned then we return the 
+        sum up till the right index from the prefix sum array */
+        return this.prefixSum[right];
+    }else{
+        //if the left index is given, then return the sum up 
+        //till the right index minus the value of prefix sum at 
+        //the index before the given left index i.e. value 
+        //at the (left - 1) index
+        return this.prefixSum[right] - this.prefixSum[left-1];
     }
-
-    return sum
-
+    
+    
 };
-
-/** 
- * Your NumArray object will be instantiated and called as such:
- * var obj = new NumArray(nums)
- * var param_1 = obj.sumRange(left,right)
- */
