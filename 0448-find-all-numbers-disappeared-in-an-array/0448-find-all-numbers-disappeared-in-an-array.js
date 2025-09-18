84ba195934a714c11a53c1cae725be78783c5135
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-18 15:12:56 +0530

    Time: 25 ms (24.81%), Space: 78.2 MB (14.13%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    
+    var set1=new Set(nums)
+    let missing=[]
+
+    for(var i=1;i<=nums.length;i++){
+        if(!set1.has(i)){
+            missing.push(i)
+        }
+    }
+return missing
+
+};
