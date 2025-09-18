7584a20b68e3497f113d89be883f183ed382a459
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-18 18:02:11 +0530

    Time: 27 ms (53.1%), Space: 59.4 MB (100%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -7,16 +7,14 @@
     
      g.sort((a, b) => a - b);
     s.sort((a, b) => a - b);
-    let i = 0, j = 0, count = 0;
 
-    while (i < g.length && j < s.length) {
-        if (s[j] >= g[i]) {
-            count++;
-            i++;
-        }
-        j++;
+    let i = 0, j = 0;
+
+    while (i < s.length && j < g.length) {
+        if (g[j] <= s[i]) j++;
+        i++;
     }
 
-    return count;
+    return j;
 
    };
