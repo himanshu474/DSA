0144c17dfe979c96231083374a0f019522f097dd
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-25 00:00:01 +0530

    Time: 11 ms (49.77%), Space: 59.3 MB (30.09%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.js
+++ 0506-relative-ranks/0506-relative-ranks.js
@@ -2,21 +2,11 @@
  * @param {number[]} score
  * @return {string[]}
  */
-var findRelativeRanks = function(score) {
-   let sorted = [...score];
-   sorted.sort((a,b)=>b-a);
-   let result = [];
-   score.forEach(element=>{
-    if(sorted.indexOf(element)==0){
-        result.push("Gold Medal")
-    }else if(sorted.indexOf(element)==1){
-        result.push("Silver Medal")
-    }else if(sorted.indexOf(element)==2){
-        result.push("Bronze Medal")
-    }else{
-        result.push(`${sorted.indexOf(element)+1}`)
-    }
-   })
-
-   return result; 
-};
+const medals = ["Fake Medal", "Gold Medal", "Silver Medal", "Bronze Medal"];
+const findRelativeRanks = (score) =>
+    score
+        .map((s, i) => ({ scored: s, position: i + 1 }))
+        .sort((x, y) => y.scored - x.scored)
+        .map(({ position }, i) => ({ position, place: i + 1 }))
+        .sort((x, y) => x.position - y.position)
+        .map(({ place }) => medals[place] || place.toString());
