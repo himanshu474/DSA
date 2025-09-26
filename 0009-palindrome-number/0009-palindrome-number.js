e1ede26c7f9b1f273380ce1f10bf420b438f490c
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-09-26 23:48:32 +0530

    Time: 5 ms (79.51%), Space: 63.9 MB (69.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var temp=0;
+    let original=x
+
+    while(x>0){
+        let digit=x%10;
+        temp=temp*10+digit
+        x=Math.floor(x / 10);
+    }
+   return temp===original
+};
