615d3db95954751212e82457e0294426e67cd397
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-10-03 18:08:45 +0530

    Time: 5 ms (33.66%), Space: 56 MB (62.75%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let ch of s) {
+        if (ch === '(' || ch === '{' || ch === '[') {
+            stack.push(ch);
+        } else {
+            if (stack.length === 0 || stack.pop() !== pairs[ch]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
