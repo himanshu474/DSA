78302e763b5016d3769a7e18e242f1869027ec48
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2025-10-16 18:09:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 0 || n === 1)
+        return 1;
+
+    return climbStairs(n - 1) + climbStairs(n - 2);
+};
