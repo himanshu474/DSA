ec6a16ffcde03dfc2a8ca40d2b95b5570513055e
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2026-04-13 11:51:26 +0530

    Time: 47 ms (43.26%), Space: 53.5 MB (80.4%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -6,11 +6,24 @@
 var mergeAlternately = function(word1, word2) {
     let res = "";
     let i = 0;
-    const minLen = Math.min(word1.length, word2.length);
+let n1=word1.length;
+let n2=word2.length;
 
-    for (i = 0; i < minLen; i++) {
-        res += word1[i] + word2[i];
+
+while(i<n1 || i <n2){
+    if(i<n1){
+        res+=word1[i]
     }
 
-    return res + word1.slice(i) + word2.slice(i);
+    if(i<n2){
+        res+=word2[i]
+    }
+
+    i++
+}
+
+return res
+
+
+
 };
