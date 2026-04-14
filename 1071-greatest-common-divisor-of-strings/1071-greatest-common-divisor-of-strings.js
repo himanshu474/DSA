8a00042d7f1c4c7334b5972c590cf08feb37e9d6
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2026-04-14 17:58:24 +0530

    Time: 0 ms (100%), Space: 54.2 MB (57.23%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    
+    if(str1+str2 !== str2+str1 ){
+        return ""
+    }
+
+
+const gcd=(a,b)=>{
+    while(b!=0){
+        let temp=b;
+    b=a%b;
+    a=temp;
+    }
+
+return a
+
+}
+
+ let gcdLength = gcd(str1.length, str2.length);
+
+    // Step 4: Return substring of that length
+    return str1.substring(0, gcdLength);
+    
+    
+    };
